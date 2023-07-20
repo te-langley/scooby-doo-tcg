9e2e7bd282bfe012da8199d794fcc2ca95175b09
Hallie Langley <hallie.langley@techelevator.com>
2023-07-19 20:32:59 -0400

    Renaming DB

--- java/src/test/java/com/techelevator/dao/TestingDatabaseConfig.java
+++ java/src/test/java/com/techelevator/dao/TestingDatabaseConfig.java
@@ -23,7 +23,7 @@ public class TestingDatabaseConfig {
     private static final String DB_PORT =
             Objects.requireNonNullElse(System.getenv("DB_PORT"), "5432");
     private static final String DB_NAME =
-            Objects.requireNonNullElse(System.getenv("DB_NAME"), "final_capstone_test");
+            Objects.requireNonNullElse(System.getenv("DB_NAME"), "scooby_doo_tcg_test");
     private static final String DB_USER =
             Objects.requireNonNullElse(System.getenv("DB_USER"), "postgres");
     private static final String DB_PASSWORD =
